4a95c6488097b7a732af184b83182ab93f8c543a
valk <Valiantsin.Kanapatski@epolsoft.com>
2023-11-22 13:53:45 +0300

    gg

--- src/widgets/Events/ui/Events.tsx
+++ src/widgets/Events/ui/Events.tsx
@@ -1,4 +1,6 @@
+import { MutableRefObject, useRef, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { useInfiniteScroll } from 'shared/lib/hook/useInfiniteScroll/useInfiniteScroll';
 import { Text } from 'shared/ui/Text/Text';
 
 import cl from './Events.module.scss';
@@ -9,34 +11,50 @@ interface EventsProps {
 }
 
 export const Events = ({ className, backgroundColor }: EventsProps) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+
+    useInfiniteScroll({
+        triggerRef,
+        callback: () => setIsVisible(true),
+    });
     
     return (
-        <div style={{background: backgroundColor}} id="events" className={classNames(cl.Events, {}, [className])}>
+        <div 
+            style={{background: backgroundColor}} 
+            id="events" 
+            className={classNames(cl.Events, {}, [className])} 
+            ref={triggerRef}
+        >
             <div className="container">
-                <Text title="Как выглядит сальса?"/>
-                <div className={cl.wrap}>
+                <div className={classNames(cl.superWrap,
+                    { [cl.active]: isVisible },
+                    [],
+                )}>
+                    <Text title="Как выглядит сальса?"/>
+                    <div className={cl.wrap}>
                     
-                    <iframe src="https://www.youtube.com/embed/u_9hLeLM7yI"
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen
-                        title="video"
-                    />
-                    <iframe src="https://www.youtube.com/embed/epWiSNsGPYc?si=ZwJ95tpv40txB2Uh"
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen
-                        title="video"
-                    />
-                    <iframe src="https://www.youtube.com/embed/zjO2AyC8wQ0?si=BFvX4-BOpr5Fyzhi"
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen
-                        title="video"
-                    />
+                        <iframe src="https://www.youtube.com/embed/u_9hLeLM7yI"
+                            frameBorder="0"
+                            allow="autoplay; encrypted-media"
+                            allowFullScreen
+                            title="video"
+                        />
+                        <iframe src="https://www.youtube.com/embed/epWiSNsGPYc?si=ZwJ95tpv40txB2Uh"
+                            frameBorder="0"
+                            allow="autoplay; encrypted-media"
+                            allowFullScreen
+                            title="video"
+                        />
+                        <iframe src="https://www.youtube.com/embed/zjO2AyC8wQ0?si=BFvX4-BOpr5Fyzhi"
+                            frameBorder="0"
+                            allow="autoplay; encrypted-media"
+                            allowFullScreen
+                            title="video"
+                        />
                     
-                </div>
-                
+                    </div>
+                </div> 
             </div>
         </div>
     );

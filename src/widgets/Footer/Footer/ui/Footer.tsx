4fc0f871d84d24f35497a7464b142380a3ffeaf5
Valentin Konopatsky <fixator03@yandex.ru>
2023-11-25 23:15:24 +0300

    fix mail

--- src/widgets/Footer/Footer/ui/Footer.tsx
+++ src/widgets/Footer/Footer/ui/Footer.tsx
@@ -10,35 +10,51 @@ import cl from './Footer.module.scss';
 
 interface FooterProps {
     className?: string;
-    backgroundColor?: string ;
+    backgroundColor?: string;
 }
 
 export const Footer = ({ className, backgroundColor }: FooterProps) => {
-
     return (
-        <footer style={{ background: backgroundColor }} id="contact" className={classNames(cl.Footer, {}, [className])}>
-            <div className="container">
+        <footer
+            style={{ background: backgroundColor }}
+            id='contact'
+            className={classNames(cl.Footer, {}, [className])}
+        >
+            <div className='container'>
                 <div className={cl.wrap}>
-                    <span className={cl.text}>© 2023 Школа танцев «СальсаБрест»</span>
+                    <span className={cl.text}>
+                        © 2023 Школа танцев «СальсаБрест»
+                    </span>
                     <div className={cl.socialWrap}>
-                        <a href={'https://www.instagram.com/salsabrest/'} aria-label="Перейти в инстаграм">
-                            <Instagram className={cl.instagram}/>
+                        <a
+                            href={'https://www.instagram.com/salsabrest/'}
+                            aria-label='Перейти в инстаграм'
+                        >
+                            <Instagram className={cl.instagram} />
                         </a>
-                        <a href={'https://t.me/salsaworlddance'} aria-label="Перейти в телеграм">
-                            <Telegram className={cl.telegram}/>
+                        <a
+                            href={'https://t.me/salsaworlddance'}
+                            aria-label='Перейти в телеграм'
+                        >
+                            <Telegram className={cl.telegram} />
                         </a>
-                        <a href={'https://www.tiktok.com/@salsabrest1'} aria-label="Перейти в тикток">
-                            <Tiktok className={cl.tiktok}/>
+                        <a
+                            href={'https://www.tiktok.com/@salsabrest1'}
+                            aria-label='Перейти в тикток'
+                        >
+                            <Tiktok className={cl.tiktok} />
                         </a>
-                        
                     </div>
                     <div className={cl.contactWrap}>
                         <a href={'tel:+375339939039'} className={cl.phoneWrap}>
-                            <Phone className={cl.phone}/>
+                            <Phone className={cl.phone} />
                             <span className={cl.text}>+ 375 33 99 39 0 39</span>
                         </a>
-                        <a href={'malito:info@salsabrest.by'} className={cl.mailWrap}>
-                            <Email className={cl.email}/>
+                        <a
+                            href='mailto:info@salsabrest.by'
+                            className={cl.mailWrap}
+                        >
+                            <Email className={cl.email} />
                             <span className={cl.text}>info@salsabrest.by</span>
                         </a>
                     </div>

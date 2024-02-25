8dae06e4c8e1e98c44cc3d6d599cc5e7668668d2
Valentin Konopatsky <fixator03@yandex.ru>
2024-02-25 16:48:51 +0300

    add hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 
@@ -8,12 +8,12 @@ import App from './app/App';
 import 'app/styles/index.scss';
 
 render(
-    <BrowserRouter>
+    <HashRouter>
         <ErrorBoundary>
             <ThemeProvider>
                 <App />
             </ThemeProvider>
         </ErrorBoundary>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root'),
 );
